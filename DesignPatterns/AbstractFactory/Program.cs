2f63d6405632b13cee2731d917a1d409ff092757
KardelRuveyda <ruveydakardelcetin@gmail.com>
2021-01-27 01:08:05 +0300

    Abstract Factory

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+    }
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            throw new NotImplementedException("Logged with log4net");
+        }
+    }
+
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            throw new NotImplementedException("Logged with nlogger");
+        }
+    }
+
+    public abstract class Caching
+    {
+        public abstract void Cache(string data);
+    }
+
+    public class MemCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with MCache");
+        }
+    }
+    public class RedisCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with MCache");
+        }
+    }
+}
